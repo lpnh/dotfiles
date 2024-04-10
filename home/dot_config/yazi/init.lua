44f4f27edd72a607537229e7bf420cd8e64f2504
lpnh <paniguel.lpnh@gmail.com>
2024-04-10 19:02:08 -0300

    Update home/.config/yazi/init.lua

--- home/dot_config/yazi/init.lua
+++ home/dot_config/yazi/init.lua
@@ -1,11 +1,18 @@
 -- https://github.com/dedukun/relative-motions.yazi?tab=readme-ov-file#configuration
-require("relative-motions"):setup({ show_numbers="relative_absolute" })
+require("relative-motions"):setup({
+	show_numbers = "relative_absolute",
+})
+
+require("zoxide"):setup({
+	update_db = true,
+})
 
 -- https://yazi-rs.github.io/docs/tips#no-status-bar
-function Status:render() return {} end
+function Status:render()
+	return {}
+end
 
 local old_manager_render = Manager.render
 function Manager:render(area)
-  return old_manager_render(self, ui.Rect { x = area.x, y = area.y, w = area.w, h = area.h + 1 })
+	return old_manager_render(self, ui.Rect({ x = area.x, y = area.y, w = area.w, h = area.h + 1 }))
 end
-
