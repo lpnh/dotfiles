035b35f80c0e4fef3a54ec6e35378233add8c839
lpnh <paniguel.lpnh@gmail.com>
2024-04-13 20:56:27 -0300

    Update home/.config/nvim/lua/plugins/cmp.lua

--- home/dot_config/nvim/lua/plugins/cmp.lua
+++ home/dot_config/nvim/lua/plugins/cmp.lua
@@ -7,7 +7,7 @@ return {
       'L3MON4D3/LuaSnip',
       {
         'L3MON4D3/LuaSnip',
-        build = 'make install_jsregexp'
+        build = 'make install_jsregexp',
       },
       'saadparwaiz1/cmp_luasnip',
 
@@ -17,17 +17,13 @@ return {
 
       -- Adds a number of user-friendly snippets
       'rafamadriz/friendly-snippets',
-    }
+    },
   },
 
   -- Autopairs
   {
     'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    opts = {} -- this is equalent to setup({}) function
+    event = 'InsertEnter',
+    opts = {}, -- this is equalent to setup({}) function
   },
-
-  -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
 }
-
