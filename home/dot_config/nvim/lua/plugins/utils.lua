1eb30489d8c0e76bf3bcabec8edfa6d4572673b5
lpnh <paniguel.lpnh@gmail.com>
2024-04-13 21:15:49 -0300

    Update home/.config/nvim/lua/plugins/utils.lua

--- home/dot_config/nvim/lua/plugins/utils.lua
+++ home/dot_config/nvim/lua/plugins/utils.lua
@@ -31,6 +31,17 @@ return {
   -- Autoformat
   {
     'stevearc/conform.nvim',
+    lazy = false,
+    keys = {
+      {
+        '<leader>f',
+        function()
+          require('conform').format { async = true, lsp_fallback = true }
+        end,
+        mode = '',
+        desc = '[F]ormat buffer',
+      },
+    },
     opts = {
       notify_on_error = false,
       format_on_save = {
