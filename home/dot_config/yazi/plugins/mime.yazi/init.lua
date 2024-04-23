fe77222ff5e95c29c1f3d8a342f760c5e53a4de6
lpnh <paniguel.lpnh@gmail.com>
2024-04-23 15:56:50 -0300

    Update home/.config/yazi/plugins/mime.yazi/init.lua

--- home/dot_config/yazi/plugins/mime.yazi/init.lua
+++ home/dot_config/yazi/plugins/mime.yazi/init.lua
@@ -1,29 +1,3 @@
---[[
-
-MIT License
-
-Copyright (c) 2024 DreamMaoMao
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-]]--
-
 local M = {}
 
 local ext_mime_map = {
@@ -1464,7 +1438,8 @@ function M:preload()
 	
 
 	if #unmatch_ext_urls then
-	  local command = Command("file"):arg("--mime-type"):stdout(Command.PIPED):stderr(Command.PIPED)
+		local file_one_path = os.getenv("YAZI_FILE_ONE") or "file"
+	  local command = Command(file_one_path):arg("--mime-type"):stdout(Command.PIPED):stderr(Command.PIPED)
 	  if ya.target_family() == "windows" then
 		command:arg("-b")
 	  else
@@ -1492,7 +1467,7 @@ function M:preload()
 	end
   
 	if #mimes then
-	  ya.manager_emit("update_mimetype", {}, mimes)
+	  ya.manager_emit("update_mimetype", { updates = mimes })
 	  return 3
 	end
 	return 2
