230a7a7de22db06645e9348e8a9fa2d401d56180
lpnh <paniguel.lpnh@gmail.com>
2024-04-25 19:53:25 -0300

    Add home/.config/nvim/lua/plugins/telescope.lua

---
+++ home/dot_config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,18 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  event = 'VimEnter',
+  branch = '0.1.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    -- If encountering errors, see telescope-fzf-native README
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make',
+      cond = function()
+        return vim.fn.executable 'make' == 1
+      end,
+    },
+    { 'nvim-telescope/telescope-ui-select.nvim' },
+    { 'nvim-tree/nvim-web-devicons' },
+  },
+}
