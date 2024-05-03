d868ce1fc5487c140e4747ad171c2b391e0c55f7
lpnh <paniguel.lpnh@gmail.com>
2024-05-03 19:19:08 -0300

    Update home/.config/nvim/lua/plugins/utils.lua

--- home/dot_config/nvim/lua/plugins/utils.lua
+++ home/dot_config/nvim/lua/plugins/utils.lua
@@ -56,8 +56,7 @@ return {
       require('which-key').register {
         ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
         ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>l'] = { name = 'Rust [L]sp', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+        ['<leader>r'] = { name = '[R]ust Lsp', _ = 'which_key_ignore' },
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
