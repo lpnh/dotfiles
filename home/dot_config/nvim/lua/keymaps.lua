7d4ecbf1a8f6cc6fd94e2d6ddad29b707b44567c
lpnh <paniguel.lpnh@gmail.com>
2024-05-03 19:41:33 -0300

    Update home/.config/nvim/lua/keymaps.lua Update home/.config/nvim/lua/plugins/utils.lua

--- home/dot_config/nvim/lua/keymaps.lua
+++ home/dot_config/nvim/lua/keymaps.lua
@@ -54,7 +54,7 @@ map.set({ 'n', 'v' }, '<leader>d', [["_d]], { desc = 'Deleting using a black hol
 map.set('n', '<C-c>', '<cmd>%y+<CR>', { desc = 'Copy entire file' })
 
 -- Split window vertically - New window on the [L]eft
-map.set('n', '<leader>L', '<C-w>v', { noremap = true, silent = true, desc = 'Split window vertically' })
+map.set('n', '<leader>l', '<C-w>v', { noremap = true, silent = true, desc = 'Split window vertically' })
 
 -- Old addiction to move lines around using `alt` key
 map.set('n', '<A-j>', '<cmd>m .+1<cr>==', { desc = 'Move down' })
--- home/dot_config/nvim/lua/plugins/utils.lua
+++ home/dot_config/nvim/lua/plugins/utils.lua
@@ -95,10 +95,6 @@ return {
       server = {
         on_attach = function(_, bufnr)
           local nmap = function(keys, func, desc)
-            if desc then
-              desc = 'LSP: ' .. desc
-            end
-
             vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
           end
 
