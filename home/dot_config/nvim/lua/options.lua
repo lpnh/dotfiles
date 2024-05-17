0703a5bfc582749b80587a2e7b41be1610742970
lpnh <paniguel.lpnh@gmail.com>
2024-05-17 07:59:16 -0300

    Update home/.config/nvim/lua/options.lua

--- home/dot_config/nvim/lua/options.lua
+++ home/dot_config/nvim/lua/options.lua
@@ -3,6 +3,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Disable unnecessary providers
+vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
