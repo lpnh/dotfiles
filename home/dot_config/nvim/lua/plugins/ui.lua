e93144a43a7b23e18d3b386c7eeacff70dbb7ae0
lpnh <paniguel.lpnh@gmail.com>
2024-05-19 20:56:00 -0300

    Update home/.config/nvim/lua/plugins/ui.lua

--- home/dot_config/nvim/lua/plugins/ui.lua
+++ home/dot_config/nvim/lua/plugins/ui.lua
@@ -2,35 +2,18 @@ return {
   -- Icons
   { 'nvim-tree/nvim-web-devicons', lazy = true },
 
-  -- Add custom colors to icons
+  -- Add custom colors to devicons
   {
-    'dgox16/devicon-colorscheme.nvim',
+    'rachartier/tiny-devicons-auto-colors.nvim',
     dependencies = {
       'nvim-tree/nvim-web-devicons',
     },
+    event = 'VeryLazy',
     config = function()
       local mocha = require('catppuccin.palettes').get_palette 'mocha'
 
-      require('devicon-colorscheme').setup {
-        colors = {
-          blue = mocha.blue,
-          cyan = mocha.teal,
-          green = mocha.green,
-          magenta = mocha.pink,
-          orange = mocha.maroon,
-          purple = mocha.mauve,
-          red = mocha.red,
-          white = mocha.subtext0,
-          yellow = mocha.yellow,
-          bright_blue = mocha.blue,
-          bright_cyan = mocha.teal,
-          bright_green = mocha.green,
-          bright_magenta = mocha.pink,
-          bright_orange = mocha.maroon,
-          bright_purple = mocha.mauve,
-          bright_red = mocha.red,
-          bright_yellow = mocha.yellow,
-        },
+      require('tiny-devicons-auto-colors').setup {
+        colors = mocha,
       }
     end,
   },
