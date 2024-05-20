8680a6e5881529e6f66a53cc9fcdbca2f23f75db
lpnh <paniguel.lpnh@gmail.com>
2024-05-20 08:36:33 -0300

    Update home/.config/nvim/lua/plugins/lsp.lua

--- home/dot_config/nvim/lua/plugins/lsp.lua
+++ home/dot_config/nvim/lua/plugins/lsp.lua
@@ -65,9 +65,9 @@ return {
 
         -- Enable inlay hints if the language server supports them
         if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-          map('<leader>th', function()
+          map('<leader>h', function()
             vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-          end, '[T]oggle Inlay [H]ints')
+          end, 'Toggle Inlay [H]ints')
         end
       end,
     })
