fae898b28c856daa3fd98ed0bb21c0327d27829f
lpnh <paniguel.lpnh@gmail.com>
2024-06-15 15:19:37 -0300

    Update home/.config/nvim/lua/plugins/utils.lua

--- home/dot_config/nvim/lua/plugins/utils.lua
+++ home/dot_config/nvim/lua/plugins/utils.lua
@@ -89,6 +89,7 @@ return {
       { '<A-1>', '<cmd>Grapple select index=1<cr>', desc = 'Select first tag' },
       { '<A-2>', '<cmd>Grapple select index=2<cr>', desc = 'Select second tag' },
       { '<A-3>', '<cmd>Grapple select index=3<cr>', desc = 'Select third tag' },
+      { '<A-4>', '<cmd>Grapple select index=4<cr>', desc = 'Select fourth tag' },
     },
   },
 
