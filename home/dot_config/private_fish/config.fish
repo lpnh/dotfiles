97d0a707836a16499be7d25d4f8cf995347779a6
lpnh <paniguel.lpnh@gmail.com>
2024-07-30 20:10:37 -0300

    Update home/.config/fish/config.fish

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -1,5 +1,11 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+# cursor style
+set -gx fish_vi_force_cursor 1
+set -gx fish_cursor_default block
+set -gx fish_cursor_insert line blink
+set -gx fish_cursor_visual block
+set -gx fish_cursor_replace_one underscore
+
+# vi bindings
+set -g fish_key_bindings fish_vi_key_bindings
 
 starship init fish | source
