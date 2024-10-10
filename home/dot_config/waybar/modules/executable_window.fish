2c6ad45fcc4b45b9b008ee3594d18dc78e6366a9
lpnh <paniguel.lpnh@gmail.com>
2024-10-10 20:16:17 -0300

    Remove home/.config/waybar/modules/window.fish Remove home/.config/waybar/modules/workspaces.fish

--- home/dot_config/waybar/modules/executable_window.fish
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env fish
-
-while true
-    sleep 1s
-
-    niri msg --json focused-window \
-        | jq -c 'if type != "object" then { "text": "", "tooltip": "" } else { "text": .app_id, "tooltip": "\(.app_id): \(.title)" } end'
-end
