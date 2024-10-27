c4ded69eeb6e8a415440d8c93ccb03c7490ffcf8
lpnh <paniguel.lpnh@gmail.com>
2024-10-27 10:41:07 -0300

    Update home/.config/wezterm/wezterm.lua

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 local config = {}
 
-config.default_prog = { 'zsh' }
+config.default_prog = { 'nu' }
 config.font = wezterm.font 'FiraMono Nerd Font'
 
 return config
