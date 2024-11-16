dce6dac3d2d702c96ee77c83c4f81c03da951049
lpnh <paniguel.lpnh@gmail.com>
2024-11-16 07:42:07 -0300

    Update home/.config/fish/functions/yz.fish

--- home/dot_config/private_fish/functions/yz.fish
+++ home/dot_config/private_fish/functions/yz.fish
@@ -3,7 +3,7 @@ function yz
 	set tmp (mktemp -t "yazi-cwd.XXXXXX")
 	yazi $argv --cwd-file="$tmp"
 	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		cd -- "$cwd"
+		builtin cd -- "$cwd"
 	end
 	rm -f -- "$tmp"
 end
