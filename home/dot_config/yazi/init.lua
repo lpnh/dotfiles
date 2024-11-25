2e2ad88006700248adbe1b3a4a034ca1fd2b3d65
lpnh <paniguel.lpnh@gmail.com>
2024-11-25 14:08:07 -0300

    Update home/.config/yazi/init.lua Update home/.config/yazi/keymap.toml

--- home/dot_config/yazi/init.lua
+++ home/dot_config/yazi/init.lua
@@ -6,3 +6,16 @@ require("relative-motions"):setup({
 require("zoxide"):setup({
 	update_db = true,
 })
+
+function Linemode:custom()
+	local year = os.date("%Y")
+	local time = (self._file.cha.mtime or 0) // 1
+
+	if time > 0 and os.date("%Y", time) == year then
+		time = os.date("%b %d %H:%M", time)
+	else
+		time = time and os.date("%b %d %Y", time) or ""
+	end
+
+	return ui.Line(string.format("%s", time))
+end
