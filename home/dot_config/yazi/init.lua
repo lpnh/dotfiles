5cb76088e60413169fd4899b14e0485a5c58ece1
lpnh <paniguel.lpnh@gmail.com>
2025-01-09 15:26:13 -0300

    Update home/.config/yazi/init.lua

--- home/dot_config/yazi/init.lua
+++ home/dot_config/yazi/init.lua
@@ -1,8 +1,3 @@
--- https://github.com/dedukun/relative-motions.yazi?tab=readme-ov-file#configuration
-require("relative-motions"):setup({
-	show_numbers = "relative_absolute",
-})
-
 require("zoxide"):setup({
 	update_db = true,
 })
