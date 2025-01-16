fafd30f0fb81f24549f77cb89dffa512eb3af7f6
lpnh <paniguel.lpnh@gmail.com>
2025-01-16 12:34:46 -0300

    Update home/.config/fish/config.fish

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -10,7 +10,7 @@ set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
 
 # fzf mocha theme
 set -gx FZF_DEFAULT_OPTS "\
---color=bg+:#313244,spinner:#f5e0dc,hl:#f38ba8 \
+--color=bg+:#313244,spinner:#f5e0dc,hl:#cba6f7 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#cba6f7 \
 --color=selected-bg:#45475a \
