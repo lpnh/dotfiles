74dc80440258fc670d8f78196281b138961a5f52
lpnh <paniguel.lpnh@gmail.com>
2025-02-01 13:37:38 -0300

    Update home/.config/fish/functions/yz.fish Update home/.config/yazi/keymap.toml

--- home/dot_config/private_fish/functions/yz.fish
+++ home/dot_config/private_fish/functions/yz.fish
@@ -1,9 +1,8 @@
 # https://yazi-rs.github.io/docs/quick-start
 function yz
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+	set cwd_file ~/.config/yazi/last_jump
+	yazi $argv --cwd-file="$cwd_file"
+	if set cwd (command cat -- "$cwd_file"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
 		builtin cd -- "$cwd"
 	end
-	rm -f -- "$tmp"
 end
