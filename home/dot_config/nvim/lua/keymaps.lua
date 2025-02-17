aa7891834c62a407d5a54fc7c07661f58e5c9a36
lpnh <paniguel.lpnh@gmail.com>
2025-02-17 17:25:29 -0300

    Update home/.config/nvim/lua/keymaps.lua

--- home/dot_config/nvim/lua/keymaps.lua
+++ home/dot_config/nvim/lua/keymaps.lua
@@ -2,9 +2,6 @@ local map = vim.keymap.set
 
 map({ 'n', 'v' }, '<space>', '<nop>', { silent = true })
 
--- Netrw
-map('n', '-', '<cmd>Explore<cr>', { silent = true })
-
 -- Favor `surround` instead
 map('n', 's', '<nop>', { noremap = true, silent = true })
 
