3ab47bc82dc0dd7ddd1e71b5b3ffc843375e6e84
lpnh <paniguel.lpnh@gmail.com>
2025-02-17 15:15:04 -0300

    Update home/.config/nvim/lua/plugins/config/snacks/dashboard.lua

--- home/dot_config/nvim/lua/plugins/config/snacks/dashboard.lua
+++ home/dot_config/nvim/lua/plugins/config/snacks/dashboard.lua
@@ -12,14 +12,14 @@ return {
     -- Set your curstom keymaps here.
     -- stylua: ignore start
     keys = {
-      { icon = ' ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
-      { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')", },
-      { icon = ' ', key = 'g', desc = 'Live Grep', action = ':FzfLua live_grep_native' },
+      { icon = ' ', key = 'j', desc = 'Jump', section = 'session' },
+      { icon = ' ', key = 'n', desc = 'New', action = ':ene | startinsert' },
+      { icon = ' ', key = 'f', desc = 'Files', action = ":lua Snacks.dashboard.pick('files')", },
+      { icon = ' ', key = 'g', desc = 'Grep', action = ':FzfLua live_grep_native' },
       { icon = ' ', key = 'o', desc = 'Oldfiles', action = ":lua Snacks.dashboard.pick('oldfiles')", },
-      { icon = '󱀸 ', key = 'l', desc = 'Last Session', section = 'session' },
-      { icon = ' ', key = 'e', desc = 'Oil', action = ':Oil' },
+      { icon = '󰏇 ', key = 'e', desc = 'Oil', action = ':Oil' },
       { icon = ' ', key = 'c', desc = 'Config', action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})", },
-      { icon = '󰒲 ', key = 'u', desc = 'Lazy Update', action = ':Lazy update', enabled = package.loaded.lazy ~= nil, },
+      { icon = '󰒲 ', key = 'u', desc = 'Update', action = ':Lazy update', enabled = package.loaded.lazy ~= nil, },
       { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
     },
     -- stylua: ignore end
