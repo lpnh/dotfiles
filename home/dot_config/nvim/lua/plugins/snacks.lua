f013250e017138e54d6840102ff52d9340239003
lpnh <paniguel.lpnh@gmail.com>
2025-03-01 04:35:04 -0300

    Update home/.config/nvim/lua/plugins/snacks.lua

--- home/dot_config/nvim/lua/plugins/snacks.lua
+++ home/dot_config/nvim/lua/plugins/snacks.lua
@@ -10,7 +10,7 @@ return {
         keys = {
           -- stylua: ignore start
           { icon = ' ', key = 'j', desc = 'Jump', section = 'session' },
-          { icon = ' ', key = 'n', desc = 'New', action = ':ene | startinsert' },
+          { icon = ' ', key = 'a', desc = 'New', action = ':ene | startinsert' },
           { icon = ' ', key = 'f', desc = 'Files', action = ":lua Snacks.dashboard.pick('files')" },
           { icon = ' ', key = 'g', desc = 'Grep', action = ':FzfLua live_grep_native' },
           { icon = ' ', key = 'o', desc = 'Oldfiles', action = ":lua Snacks.dashboard.pick('oldfiles')" },
