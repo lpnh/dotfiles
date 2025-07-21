eab0d816b280bed4cc26393f67427594d34a0d0e
lpnh <paniguel.lpnh@gmail.com>
2025-07-21 15:39:12 -0300

    Update home/.config/nvim/lua/plugins/treesitter.lua

--- home/dot_config/nvim/lua/plugins/treesitter.lua
+++ home/dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,102 +1,100 @@
 return {
-  {
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
-    build = ':TSUpdate',
-    lazy = false,
-    main = 'nvim-treesitter.configs',
-    opts = {
-      ensure_installed = {
-        'bash',
-        'c',
-        'css',
-        'diff',
-        'dockerfile',
-        'fish',
-        'gotmpl',
-        'html',
-        'htmldjango', -- TODO: htmlrinja
-        'json',
-        'jsonc',
-        'just',
-        'kdl',
-        'lua',
-        'luadoc',
-        'markdown',
-        'markdown_inline',
-        'query',
-        'regex',
-        'ron',
-        'rust',
-        'sql',
-        'toml',
-        'tsx', -- required by treesitter itself (?)
-        'vim',
-        'vimdoc',
-        'yaml',
+  'nvim-treesitter/nvim-treesitter',
+  dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
+  build = ':TSUpdate',
+  lazy = false,
+  main = 'nvim-treesitter.configs',
+  opts = {
+    ensure_installed = {
+      'bash',
+      'c',
+      'css',
+      'diff',
+      'dockerfile',
+      'fish',
+      'gotmpl',
+      'html',
+      'htmldjango', -- TODO: htmlrinja
+      'json',
+      'jsonc',
+      'just',
+      'kdl',
+      'lua',
+      'luadoc',
+      'markdown',
+      'markdown_inline',
+      'query',
+      'regex',
+      'ron',
+      'rust',
+      'sql',
+      'toml',
+      'tsx', -- required by treesitter itself (?)
+      'vim',
+      'vimdoc',
+      'yaml',
+    },
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<C-space>',
+        node_incremental = '<C-space>',
+        scope_incremental = '<C-s>',
+        node_decremental = '<M-space>',
       },
-      auto_install = true,
-      highlight = {
+    },
+    textobjects = {
+      select = {
         enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      incremental_selection = {
-        enable = true,
+        lookahead = true, -- automatically jump forward to textobj
         keymaps = {
-          init_selection = '<C-space>',
-          node_incremental = '<C-space>',
-          scope_incremental = '<C-s>',
-          node_decremental = '<M-space>',
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
+          ['ab'] = '@block.outer',
+          ['ib'] = '@block.inner',
+          ['aa'] = '@parameter.outer',
+          ['ia'] = '@parameter.inner',
         },
+        include_surrounding_whitespace = true,
       },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true, -- automatically jump forward to textobj
-          keymaps = {
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@class.outer',
-            ['ic'] = '@class.inner',
-            ['ab'] = '@block.outer',
-            ['ib'] = '@block.inner',
-            ['aa'] = '@parameter.outer',
-            ['ia'] = '@parameter.inner',
-          },
-          include_surrounding_whitespace = true,
+      move = {
+        enable = true,
+        set_jumps = true,
+        goto_next_start = {
+          [']a'] = '@parameter.outer',
+          [']f'] = '@function.outer',
+          [']c'] = '@class.outer',
         },
-        move = {
-          enable = true,
-          set_jumps = true,
-          goto_next_start = {
-            [']a'] = '@parameter.outer',
-            [']f'] = '@function.outer',
-            [']c'] = '@class.outer',
-          },
-          goto_next_end = {
-            [']A'] = '@parameter.outer',
-            [']F'] = '@function.outer',
-            [']C'] = '@class.outer',
-          },
-          goto_previous_start = {
-            ['[a'] = '@parameter.outer',
-            ['[f'] = '@function.outer',
-            ['[c'] = '@class.outer',
-          },
-          goto_previous_end = {
-            ['[A'] = '@parameter.outer',
-            ['[F'] = '@function.outer',
-            ['[C'] = '@class.outer',
-          },
-          goto_next = {
-            [']q'] = '@conditional.outer',
-          },
-          goto_previous = {
-            ['[q'] = '@conditional.outer',
-          },
+        goto_next_end = {
+          [']A'] = '@parameter.outer',
+          [']F'] = '@function.outer',
+          [']C'] = '@class.outer',
+        },
+        goto_previous_start = {
+          ['[a'] = '@parameter.outer',
+          ['[f'] = '@function.outer',
+          ['[c'] = '@class.outer',
+        },
+        goto_previous_end = {
+          ['[A'] = '@parameter.outer',
+          ['[F'] = '@function.outer',
+          ['[C'] = '@class.outer',
+        },
+        goto_next = {
+          [']q'] = '@conditional.outer',
+        },
+        goto_previous = {
+          ['[q'] = '@conditional.outer',
         },
       },
     },
-    config = function(_, opts) require('nvim-treesitter.configs').setup(opts) end,
   },
+  config = function(_, opts) require('nvim-treesitter.configs').setup(opts) end,
 }
