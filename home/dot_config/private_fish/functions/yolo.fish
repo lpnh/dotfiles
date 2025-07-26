c39d288d12a201448be20d83ebdc2b25b70a233e
lpnh <paniguel.lpnh@gmail.com>
2025-07-26 16:19:54 -0300

    Add .config/fish/functions/yolo.fish

---
+++ home/dot_config/private_fish/functions/yolo.fish
@@ -0,0 +1,33 @@
+function yolo
+    set -l commands \
+        "rustup update" \
+        "cargo install-update --all" \
+        "mise self-update --yes" \
+        "mise upgrade --yes" \
+        "npm update --global" \
+        "ya pkg upgrade" \
+        "tldr --update"
+
+    set -l green (set_color green)
+    set -l red (set_color red)
+    set -l reset (set_color normal)
+    set -l failed_commands
+
+    for cmd in $commands
+        if not eval $cmd
+            set -a failed_commands "$cmd"
+        end
+        echo
+    end
+
+    echo
+
+    if test (count $failed_commands) -eq 0
+        echo $green"✨ やった!"$reset
+    else
+        echo $red"Manual intervetion required for the following command(s):"$reset
+        for failed in $failed_commands
+            echo "  ✗ $failed"
+        end
+    end
+end
