5f98677d80fad66cb3b7010d519f0142336c9cc1
lpnh <paniguel.lpnh@gmail.com>
2025-08-14 06:59:22 -0300

    Update .config/nvim/lua/plugins/catppuccin.lua

--- home/dot_config/nvim/lua/plugins/catppuccin.lua
+++ home/dot_config/nvim/lua/plugins/catppuccin.lua
@@ -1,13 +1,15 @@
--- Color references: https://github.com/catppuccin/nvim/blob/main/lua/catppuccin/palettes/mocha.lua
 -- Integrations: https://github.com/catppuccin/nvim?tab=readme-ov-file#integrations
 return {
   'catppuccin/nvim',
   name = 'catppuccin',
   priority = 1000,
-  lazy = false,
   config = function()
     require('catppuccin').setup {
       transparent_background = true,
+      float = {
+        transparent = true,
+        solid = false,
+      },
       custom_highlights = function(colors)
         return {
           -- Comment = { fg = colors.surface2 },
@@ -19,7 +21,6 @@ return {
         }
       end,
       integrations = {
-        blink_cmp = true,
         noice = true,
         snacks = { enabled = true, indent_scope_color = 'surface2' },
         which_key = true,
