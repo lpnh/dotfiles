9e761586ec3ca1369a54fefeda8022c9a3a78168
lpnh <paniguel.lpnh@gmail.com>
2025-08-19 21:16:28 -0300

    Update .config/nvim/lua/plugins/snacks.lua

--- home/dot_config/nvim/lua/plugins/snacks.lua
+++ home/dot_config/nvim/lua/plugins/snacks.lua
@@ -10,12 +10,11 @@ return {
         keys = {
           -- stylua: ignore start
           { icon = ' ', key = 'j', desc = 'Jump', section = 'session' },
-          { icon = ' ', key = 'a', desc = 'New', action = ':ene | startinsert' },
           { icon = ' ', key = 'f', desc = 'Files', action = ":lua Snacks.dashboard.pick('files')" },
           { icon = ' ', key = 'g', desc = 'Grep', action = ':FzfLua live_grep_native' },
-          { icon = ' ', key = 'o', desc = 'Oldfiles', action = ":lua Snacks.dashboard.pick('oldfiles')" },
           { icon = '󰏇 ', key = 'e', desc = 'Oil', action = ':Oil' },
           { icon = ' ', key = 'c', desc = 'Config', action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = '󰹩 ', key = 't', desc = 'TSUpdate', action = ':TSUpdate' },
           { icon = '󰒲 ', key = 'u', desc = 'Update', action = ':Lazy update', enabled = package.loaded.lazy ~= nil },
           { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
           -- stylua: ignore end
