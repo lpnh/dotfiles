57580fadde0643511fe1937582d24086cc510b6f
lpnh <paniguel.lpnh@gmail.com>
2026-02-05 07:55:06 -0300

    Update .config/fish/conf.d/aliases.fish Update .config/fish/functions/cn.fish

--- home/dot_config/private_fish/functions/cn.fish
+++ home/dot_config/private_fish/functions/cn.fish
@@ -3,5 +3,8 @@ function cn
     cargo new $repo --vcs=none
     builtin cd $repo
     echo "/target" >> .gitignore
+    echo "# $repo" >> README.md
     jj git init
+    jj bookmark create main
+    jj describe -m "init"
 end
